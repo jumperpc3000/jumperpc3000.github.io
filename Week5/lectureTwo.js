9d9d2ee1373eb140134559a687a326914e98a1b3
jumperpc3000 <43353058+jumperpc3000@users.noreply.github.com>
2026-04-19 20:10:30 -0700

    Lecture Two

---
+++ Week5/lectureTwo.js
@@ -0,0 +1,40 @@
+// Javascript allows for the manipulation of data 
+// allows us to change HTML 
+// allows us to change CSS 
+
+// The data types that we use are:  number, string, arrays and objects 
+// to figure out what type of data type we are working with, console.log(typeof variableName)
+// We are going to introduce math operators 
+
+// specifically for the number data types 
+
+let a = 2
+let b = 10 
+let c 
+
+// first one is the add operator (addition) and it uses the (+) symbol. For math operation (meaning all values are of type number) this is addition. 
+
+// if it's string and number, it combines them into one sentence (for display purposes)
+
+console.log(`A + B = ${a+b}\n Use with literals: ${4+5}`)
+
+// subtraction uses the (-) symbol 
+console.log(`A-B = ${a-b}`)
+
+// division uses the (/) symbol 
+
+console.log(`A/B is ${a/b}`)
+
+// modulo uses the (%) symbol, and it returns the REMAINDER of a quotient 
+
+console.log(`B%A = 0.  Evaluation: ${b%a}`)
+console.log(`What will return something is: 11%2 (1) - ${11%2}`)
+
+// multiplication uses the (*) symbol to multiply values 
+
+console.log(`A*B = ${a*b}`)
+
+// exponents are defined by (**) operator  DO NOT USE ^ 
+
+console.log(`A**B = ${a**b}`)
+console.log(`A^B = ${a^b}`)
